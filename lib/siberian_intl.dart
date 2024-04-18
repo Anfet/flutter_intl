288c0b19021ce2bafbff1c445c42c1b8c257762a
Oleg Toplionkin <oleg.toplionkin@gmail.com>
2024-04-19 01:36:33 +0300

    * добавлены загрузчики в экспорт

--- lib/siberian_intl.dart
+++ lib/siberian_intl.dart
@@ -6,3 +6,7 @@ export 'src/translated_string.dart';
 export 'src/translator.dart';
 export 'src/types.dart';
 export 'src/ui/translated_widget.dart';
+export 'src/loaders/sources/translation_loader_source.dart';
+export 'src/loaders/translation_loader.dart';
+export 'src/loaders/csv_translation_loader.dart';
+export 'src/loaders/json_translation_loader.dart';
